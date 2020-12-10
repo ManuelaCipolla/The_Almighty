077bc55fe9d76c96167848d6a3a853fa0481d3ea
ManuelaCipolla <74646639+ManuelaCipolla@users.noreply.github.com>
2020-12-10 19:09:24 +0100

    Player1 and Player2
    Made player 1 and controlled it
    player 2 can spawn
    player 2 can be controlled

---
+++ The_Almighty/Assets/Scripts/Player1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    [SerializeField]
+    private GameObject _player2;
+    private bool p2alive = false;
+    
+    void Start()
+    {
+        transform.position = new Vector3(-2, -3.5f, 0);
+    }
+
+    
+    void Update()
+    {
+        Movement();
+
+        //Boundaries of screen
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -5, 5),0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3.5f, 3.5f), transform.position.y, 0);
+
+        if(Input.GetKey(KeyCode.P) && p2alive != true)
+        {
+            Player2();
+        }
+
+    }
+
+    void Movement() //Player movement
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal1");
+        float VerticalInput = Input.GetAxis("Vertical1");
+
+        Vector3 direction = new Vector3 (HorizontalInput, VerticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+
+    void Player2()
+    {
+        Instantiate(_player2, transform.position + new Vector3(4, 0, 0), Quaternion.identity);
+        p2alive = true;
+    }
+}
---
+++ The_Almighty/Assets/Scripts/Player2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+    void Start()
+    {
+        transform.position = new Vector3(2, -3.5f, 0);
+    }
+
+
+    void Update()
+    {
+        Movement();
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -5, 5),0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3.5f, 3.5f), transform.position.y, 0);
+    }
+
+    void Movement() //Player movement
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal2");
+        float VerticalInput = Input.GetAxis("Vertical2");
+
+        Vector3 direction = new Vector3 (HorizontalInput, VerticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+}
