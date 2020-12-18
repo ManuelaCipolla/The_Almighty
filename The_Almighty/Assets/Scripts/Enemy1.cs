22103184aa672a434ab5c4660494d6e11e657a25
ManuelaCipolla <74646639+ManuelaCipolla@users.noreply.github.com>
2020-12-18 18:37:48 +0100

    Spawn enemies
    enemies can spawn
    spawn manage
    made enemies prefab
    problem with direction of enemies.

---
+++ The_Almighty/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= 4.46f)
+        {
+            if(transform.position.y >= -4.46f)
+            {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+            }
+        }
+
+        else if (transform.position.y > -4.47f)
+        {
+            if(transform.position.y <= 4.47f)
+            {
+                transform.Translate(Vector3.up * _speed * Time.deltaTime);
+            }
+        }
+
+        else if (transform.position.x <= 3.22f)
+        {
+            if (transform.position.x >= -3.22f)
+            {
+                transform.Translate(Vector3.left * _speed * Time.deltaTime);
+            }
+        }
+
+        else if (transform.position.x >= -3.23f)
+        {
+            if (transform.position.x <= 3.23f)
+            {
+                transform.Translate(Vector3.right * _speed * Time.deltaTime);
+            }
+        }
+    }
+
+}
